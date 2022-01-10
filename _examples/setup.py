d6be4fb4dea215ed0193a2f5ca20d10c92b549f2
Jacopo <jacopo.degattis@gmail.com>
2022-01-10 16:39:11 +0100

    Add methods to download different types of resources from github such as files, blobs, fodlers etc.

--- _examples/setup.py
+++ _examples/setup.py
@@ -2,6 +2,7 @@ import os
 import base64
 import requests
 
+ROOT_PATH = ".tmp/"
 MAIN_REPO = "https://api.github.com/repos/jacopo-degattis/ftext/contents/"
 
 # TODO: add caching for repo response
@@ -30,16 +31,77 @@ def list_extensions():
 
     return json_content
 
+def handle_dir(resource, library):
+    
+    if not os.path.isdir(f"{ROOT_PATH}{library}/{resource['name']}"):
+        os.mkdir(f"{ROOT_PATH}{library}/{resource['name']}")
+
+    response = requests.get(resource['url'])
+    
+    if response.status_code != 200:
+        print("ERROR: Error while fetching uri")
+    
+    next_resources = response.json()
+    
+    for res in next_resources:
+        download_resource(res, library)
+
+def handle_blob(resource, library):
+    # TODO
+    pass
+    
+def handle_file(resource, library):
+    download_uri = resource['download_url']
+    
+    response = requests.get(download_uri)
+    
+    if response.status_code != 200:
+        print("ERROR: encountered an error while fetching file ", resource['name'])
+        
+    file_bytes = response.content
+    
+    with open(ROOT_PATH + resource['path'], "wb") as output:
+        output.write(file_bytes)
+        output.close()
+    
+def handle_tree(resource, library):
+    # TODO
+    pass
+
+def download_resource(resource, library):
+    mapping = {
+        'dir': handle_dir,
+        'blob': handle_blob,
+        'tree': handle_tree,
+        'file': handle_file
+    }
+    
+    print(type(resource), library)
+    
+    return mapping[resource['type']](resource, library)
+
 def download_extension(index, extensions):
-    extension_name = extensions[index-1]
+    extension_name = extensions[index-2]['name']
 
     download_uri = MAIN_REPO + extension_name
+    response = requests.get(download_uri)
+    
+    if response.status_code != 200:
+        print("ERROR: error while requesting uri, status code = ", response.status_code)
+        
+    data = response.json()
+    
+    for resource in data:
+        download_resource(resource, extension_name)
 
 if __name__ == "__main__":
 
     if not os.environ.get("GIT_USER") or not os.environ.get("GIT_TOKEN"):
         print("ERROR: GIT_USER or GIT_TOKEN has not been provided as ENV variable")
         exit(1)
+
+    if not os.path.isdir(ROOT_PATH):
+        os.mkdir(ROOT_PATH)
 
     print("** Avaiable extensions for flask-template-extensions")
 
@@ -49,6 +111,9 @@ if __name__ == "__main__":
     if choice > len(extensions) or choice < 0:
         print("ERROR: Invalid extensions index provided")
         exit(1)
+
+    if not os.path.isdir(ROOT_PATH + extensions[choice-2]['name']):
+        os.mkdir(ROOT_PATH + extensions[choice-2]['name'])
 
     download_extension(choice, extensions)
 
